00ab1132d028130465cb6d7d9fa27ab298e39b3d
jacky760828 <jacky.0828@hotmail.com>
2023-10-25 00:50:54 +0800

    10/25
    demo

--- jq.js
+++ jq.js
@@ -1,18 +1,85 @@
 // import Swiper from 'https://cdn.jsdelivr.net/npm/swiper@10/swiper-bundle.min.mjs'
+var percent=0;
+var move_count=0; 
+var change=0;
+$(function(){
+    
+      $('.right').click(function(){
+       
+        if(move_count<2)
+        {
+         move_count+=1; 
+          $('.pictures').css('left',-1460*move_count);
+        
+        }
+      })
+
+      $('.left').click(function(){
+
+        if(move_count>0)
+        {
+            move_count-=1; 
+            $('.pictures').css('left',-1460*move_count);
+        }
+
+      })
+
+
+        //點擊時按鈕加上 class
+        $('.sel li').click(function(){
+          $('.sel li').removeClass('btnNow');
+          $(this).addClass('btnNow');
+        })  
+})
+
+
+new Swiper('.swiper-container', {
+  nextButton: '.swiper-button-next',
+  prevButton: '.swiper-button-prev',
+  slidesPerView: 3,
+  slidesPerGroup: 3,
+  centeredSlides: true,
+  watchOverflow: true,
+  spaceBetween: 30,
+  parallax : true,
+  grabCursor : true,
+  loop: true,
+  //autoplay:true,
+  loopedSlides: 4,
+  watchSlidesVisibility: true,
+  breakpoints: {
+
+      1028: {
+          slidesPerView:5,
+          spaceBetween: 30,
+      },
+      480: {
+          slidesPerView:1,
+          spaceBetween: 10,
+      }
+  }
+});
 
 
 
-$(document).ready(function() {
-	const swiper = new Swiper('.swiper', {
+
+
+
+
+
+
+// $(document).ready(function() {
+	
+/*const swiper = new Swiper('.swiper', {
   // Optional parameters
   direction: 'horizontal',
   effect: "fade",
   // speed:1000,
    loop: true,
-   autoplay: {
-    delay: 3000,
-    disableOnInteraction: false,
-  },
+  //  autoplay: {
+    // delay: 3000,
+    // disableOnInteraction: false,
+  // },
 
 
  // effect:'cube',
@@ -27,11 +94,37 @@ $(document).ready(function() {
     prevEl: '.swiper-button-prev',
   },
  });
-lightbox.option({
-        'resizeDuration': 2000,
-        'wrapAround': true
-    })
-});
+
+
+ var swiper2 = new Swiper('.swiper-container', {
+  nextButton: '.swiper-button-next',
+  prevButton: '.swiper-button-prev',
+  slidesPerView: 4,
+  centeredSlides: true,
+  spaceBetween: 30,
+  loop: true,
+  loopedSlides: 5,
+  watchSlidesVisibility: true,
+  breakpoints: {
+
+      1028: {
+          slidesPerView:3,
+          spaceBetween: 30,
+      },
+      480: {
+          slidesPerView:1,
+          spaceBetween: 10,
+      }
+  }
+});*/
+
+
+
+
+
+
+
+
 
 
 
